f85d27341e79574f87a3e48d96a36708be3e7059
shardul-2007 <shardulparihar2007@gmail.com>
2026-03-05 21:46:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function showForm() {
-document.getElementById("applyForm").style.display = "block";
-}
-
-document.getElementById("scholarshipForm").addEventListener("submit", function(e){
-
-e.preventDefault();
-
-document.getElementById("successMsg").innerHTML =
-"Application submitted successfully!";
-
-this.reset();
-
-});
+console.log("Scholarship Website Loaded");
