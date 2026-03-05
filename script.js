2f422758507bd5d42377267e68a2cb6ef68ee601
shardul-2007 <shardulparihar2007@gmail.com>
2026-03-05 21:58:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log("Scholarship Website Loaded");
+document.addEventListener("DOMContentLoaded", function(){
+
+const form = document.getElementById("appForm");
+
+if(form){
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let name = document.getElementById("name").value;
+
+let id = "SCH" + Math.floor(Math.random()*100000);
+
+localStorage.setItem(id,name);
+
+document.getElementById("appID").innerHTML =
+"Application Submitted. Your ID: " + id;
+
+});
+
+}
+
+});
+
+function checkStatus(){
+
+let id = document.getElementById("searchID").value;
+
+let data = localStorage.getItem(id);
+
+if(data){
+
+document.getElementById("result").innerHTML =
+"Application Found: " + data + " (Under Review)";
+
+}
+
+else{
+
+document.getElementById("result").innerHTML =
+"Application ID Not Found";
+
+}
+
+}
